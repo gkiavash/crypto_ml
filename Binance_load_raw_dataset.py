203b444781fb63db007b96545fc411a5587ce3af
gkiavash <gkiavash@gmail.com>
2021-11-08 16:46:30 +0100

    add volume to raw dataset, new config

--- Binance_load_raw_dataset.py
+++ Binance_load_raw_dataset.py
@@ -8,11 +8,12 @@ from binance.client import Client
 import config as current_config
 
 
-def get_data(current_config=current_config):
-    if os.path.exists(current_config.RAW_DATASET_FULL_PATH):
-        print('*** raw dataset exists ***')
-        logging.info('*** raw dataset exists ***')
-        return
+def get_data(current_config=current_config, overwrite=False):
+    if not overwrite:
+        if os.path.exists(current_config.RAW_DATASET_FULL_PATH):
+            print('*** raw dataset exists ***')
+            logging.info('*** raw dataset exists ***')
+            return
 
     api_key = 'm7sioX7ArO9qtdtPuOH6VRIkqiz2Dqki4A8FUqF7zheLvgogdux1UZ9KoHGNj3bT'
     api_secret = 'oqzZkc4nlvq77uUfs5L08sWYKxlpk32MmJ0su3yLLkARx2YvDGS4CAHQ3TFvqGOv'
@@ -33,8 +34,10 @@ def get_data(current_config=current_config):
             'high': klines_[:, 2],
             'low': klines_[:, 3],
             'close': klines_[:, 4],
+            'volume': klines_[:, 5],
         }
     )
     df['symbol'] = current_config.SYMBOL
 
+    logging.info('Saving raw data in {}'.format(current_config.RAW_DATASET_FULL_PATH))
     df.to_csv(current_config.RAW_DATASET_FULL_PATH, encoding='utf-8', index=False)
