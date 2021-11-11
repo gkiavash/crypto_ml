a19510d3443587e233b7c4321d4fa81139337837
gkiavash <gkiavash@gmail.com>
2021-11-11 13:37:01 +0100

    more indicators: counters, m, better logging

--- Binance_indicators.py
+++ Binance_indicators.py
@@ -1,4 +1,6 @@
 import logging
+
+import pandas
 import pandas as pd
 
 from ta.utils import dropna
@@ -110,7 +112,8 @@ def add_indicators_BollingerBands(df, col_names, window=21, window_dev=2, ones_r
 
     # Add Width Size Bollinger Bands
     # df['bb_bbw'] = indicator_bb.bollinger_wband().round(6)
-    df['bb_bbp'] = indicator_bb.bollinger_pband().round(4)
+    df['bb_bbp'] = indicator_bb.bollinger_pband().round(3)
+
     if ones_reverse:
         df.insert(1, 'ones', 1)
         df['bb_bbp'] = (df['ones']/df['bb_bbp']).round(4).abs().apply(lambda x: x if x < 10000 else 10000)
@@ -120,7 +123,11 @@ def add_indicators_BollingerBands(df, col_names, window=21, window_dev=2, ones_r
 
 
 def add_indicators_RSIIndicator(df, col_names, window=6, ones_reverse=False):
-    df['rsi_{}_'.format(window)] = RSIIndicator(close=df[col_names['close']], window=window).rsi().round(2)
+    df[f'rsi_{window}_'] = RSIIndicator(close=df[col_names['close']], window=window).rsi().round(2)
+
+    logging.info(f'for rsi_{window}_')
+    logging.info(df[df[f'rsi_{window}_'] > 60].groupby(['signal_buy']).count())
+
     if ones_reverse:
         df.insert(1, 'ones', 1)
         df['rsi_{}_'.format(window)] = (df['ones']/df['rsi_{}_'.format(window)]).round(4)
@@ -135,26 +142,19 @@ def add_indicators_EMAIndicator(df, col_names, w1=7, w2=25, w3=99, extra=None):
     df['ema_{}_'.format(w3)] = EMAIndicator(close=df[col_names['close']], window=w3).ema_indicator().round(2)
 
     if extra == 'binary':
-        df['ema_{}_{}'.format(w1, w2)] = df.apply(
-            lambda row: 1 if row['ema_{}_'.format(w1)] > row['ema_{}_'.format(w2)] else 0, axis=1
-        ).astype('category')
-        df['ema_{}_{}'.format(w1, w3)] = df.apply(
-            lambda row: 1 if row['ema_{}_'.format(w1)] > row['ema_{}_'.format(w3)] else 0, axis=1
-        ).astype('category')
-        df['ema_{}_{}'.format(w2, w3)] = df.apply(
-            lambda row: 1 if row['ema_{}_'.format(w2)] > row['ema_{}_'.format(w3)] else 0, axis=1
-        ).astype('category')
+        df[f'ema_{w1}_{w2}'] = df.apply(lambda row: 1 if row[f'ema_{w1}_'] > row[f'ema_{w2}_'] else 0, axis=1)\
+            .astype('category')
+        df[f'ema_{w1}_{w3}'] = df.apply(lambda row: 1 if row[f'ema_{w1}_'] > row[f'ema_{w3}_'] else 0, axis=1)\
+            .astype('category')
+        df[f'ema_{w2}_{w3}'] = df.apply(lambda row: 1 if row[f'ema_{w2}_'] > row[f'ema_{w3}_'] else 0, axis=1)\
+            .astype('category')
 
-    if extra == 'propostional':
-        df['ema_{}_{}'.format(w1, w2)] = (df['ema_{}_'.format(w1)] / df['ema_{}_'.format(w2)]).round(3)
-        df['ema_{}_{}'.format(w1, w3)] = (df['ema_{}_'.format(w1)] / df['ema_{}_'.format(w3)]).round(3)
-        df['ema_{}_{}'.format(w2, w3)] = (df['ema_{}_'.format(w2)] / df['ema_{}_'.format(w3)]).round(3)
+    elif extra == 'propostional':
+        df[f'ema_{w1}_{w2}'] = (df[f'ema_{w1}_'] / df[f'ema_{w2}_']).round(3)
+        df[f'ema_{w1}_{w3}'] = (df[f'ema_{w1}_'] / df[f'ema_{w3}_']).round(3)
+        df[f'ema_{w2}_{w3}'] = (df[f'ema_{w2}_'] / df[f'ema_{w3}_']).round(3)
     if extra is not None:
-        df.drop([
-            'ema_7_',
-            'ema_25_',
-            'ema_99_',
-        ], axis=1, inplace=True)
+        df.drop(['ema_7_', 'ema_25_', 'ema_99_'], axis=1, inplace=True)
     print('Done: ', 'EMA')
     return df
 
@@ -177,10 +177,8 @@ def add_indicators_PSARIndicator(df, col_names):
         max_step=0.2
     ).psar().round(2)
 
-    df['sar_'] = df.apply(
-        lambda row: 1 if row['sar'] < row[col_names['close']] else 0,
-        axis=1
-    ).astype('category')
+    df['sar_'] = df.apply(lambda row: 1 if row['sar'] < row[col_names['close']] else 0, axis=1).astype('category')
+
     df.drop(['sar'], axis=1, inplace=True)
     print('Done: ', 'SAR')
     return df
@@ -206,18 +204,94 @@ def add_indicators_candle(df, col_names):
     return df
 
 
+def add_m(df, column, shift, need_fraction):
+    df[f'{column}_{shift}'] = df[column].shift(shift)
+    if not need_fraction:
+        df[f'{column}_{shift}_m'] = df.apply(lambda row: (row[column] - row[f'{column}_{shift}']), axis=1).round(4)
+    else:
+        df[f'{column}_{shift}_m'] = df.apply(
+            lambda row: (row[column] - row[f'{column}_{shift}']) / row[f'{column}_{shift}'], axis=1).round(4)
+
+    logging.info(f'for {column}_{shift}_m < 0')
+    logging.info(df[df[f'{column}_{shift}_m'] < 0].groupby(['signal_buy']).count())
+
+    logging.info(f'for {column}_{shift}_m > 0')
+    logging.info(df[df[f'{column}_{shift}_m'] > 0].groupby(['signal_buy']).count())
+    # df.drop([f'{column}_{shift}'], axis=1, inplace=True)
+    return df
+
+
+def add_counters(df):
+    rsi_window = 6
+    # df[f'rsi_{rsi_window}_count_{20}_rows'] = sum([1 if df[f'rsi_{rsi_window}_'].shift(i) <= 40 else 0 for i in range(21)])
+    # df[f'bb_bbp_count_{20}_rows'] = sum([1 if df['bb_bbp'].shift(i) <= 0.5 else 0 for i in range(21)])
+
+    col_names = []
+    for i in range(21):
+        col_names.append(f'rsi_{rsi_window}_count_{i}_rows')
+        df[f'rsi_{rsi_window}_count_{i}_rows'] = df[f'rsi_{rsi_window}_'].shift(i)
+    df[f'rsi_{rsi_window}_count'] = df.apply(lambda row: sum([1 if row[c] <= 40 else 0 for c in col_names]), axis=1)
+    df.drop(col_names, axis=1, inplace=True)
+
+    col_names = []
+    for i in range(21):
+        col_names.append(f'bb_bbp_count_{i}_rows')
+        df[f'bb_bbp_count_{i}_rows'] = df['bb_bbp'].shift(i)
+    df[f'bb_bbp_count_count'] = df.apply(lambda row: sum([1 if row[c] <= 0.5 else 0 for c in col_names]), axis=1)
+    df.drop(col_names, axis=1, inplace=True)
+
+    return df
+
+
+def df_logging(df, INPUT_DATASET_PATH):
+    if df is None:
+        df = pd.read_csv(INPUT_DATASET_PATH, delimiter=",")
+
+    logging.info(f'for bb_bbp < 0.4')
+    logging.info(df[df[f'bb_bbp'] < 0.4].groupby(['signal_buy']).count())
+
+    logging.info(f'for bb_bbp < 0.3')
+    logging.info(df[df[f'bb_bbp'] < 0.3].groupby(['signal_buy']).count())
+
+    logging.info(f'for bb_bbp < 0.2')
+    logging.info(df[df[f'bb_bbp'] < 0.3].groupby(['signal_buy']).count())
+
+    logging.info('for sar_')
+    logging.info(df[df['sar_'] == 1].groupby(['signal_buy']).count())
+
+    w1 = 7
+    w2 = 25
+    w3 = 99
+
+    logging.info(f'for ema_{w1}_{w2}')
+    logging.info(df[df[f'ema_{w1}_{w2}'] == 1].groupby(['signal_buy']).count())
+
+    logging.info(f'for ema_{w1}_{w3}')
+    logging.info(df[df[f'ema_{w1}_{w3}'] == 1].groupby(['signal_buy']).count())
+
+    logging.info(f'for ema_{w2}_{w3}')
+    logging.info(df[df[f'ema_{w2}_{w3}'] == 1].groupby(['signal_buy']).count())
+
+    logging.info(f'for ema_{w1}_{w2}_{w3}')
+    logging.info(df[(df[f'ema_{w1}_{w2}'] == 1) &
+                    (df[f'ema_{w2}_{w3}'] == 1) &
+                    (df[f'ema_{w1}_{w3}'] == 1)].groupby(['signal_buy']).count())
+
+
 def add_indicators_v2(df, INPUT_DATASET_PATH, OUTPUT_FULL_PATH, return_df, col_names):
-    logging.info("###### " + OUTPUT_FULL_PATH + " ######")
+    logging.info("###### Adding Indicators ######")
     if df is None:
         df = pd.read_csv(INPUT_DATASET_PATH, header=0, delimiter=",")
         logging.info('dataset found: {}'.format(df.columns.tolist()))
     df.dropna(inplace=True)
 
+    # df = add_indicators_cs1(df, col_names)
     df = add_indicators_cs2(df, col_names)
 
     if return_df:
         return df
 
+    logging.info(f"###### Saving to {OUTPUT_FULL_PATH} ######")
     df.set_index('unix', inplace=True)
     df.to_csv(
         OUTPUT_FULL_PATH,
@@ -241,8 +315,11 @@ def add_indicators_cs2(df, col_names):
     df = add_indicators_BollingerBands(df, col_names, window=21, window_dev=2, ones_reverse=False)
     df = add_indicators_RSIIndicator(df, col_names, window=6, ones_reverse=False)
     df = add_indicators_EMAIndicator(df, col_names, w1=7, w2=25, w3=99, extra='binary')
-    df = add_indicators_MACDIndcator(df, col_names, window_slow=26, window_fast=12, window_sign=9)
+    # df = add_indicators_MACDIndcator(df, col_names, window_slow=26, window_fast=12, window_sign=9)
     df = add_indicators_PSARIndicator(df, col_names)
     df = add_indicators_hour_percent_profit(df, col_names)
     # df = add_indicators_candle(df, col_names)
+    # df = add_m(df, column='bb_bbp', shift=3, need_fraction=False)
+    df = add_counters(df)
+
     return df
